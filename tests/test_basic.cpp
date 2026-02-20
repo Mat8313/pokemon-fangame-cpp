53177e317411b34b85fe553254fb7174b808d0c2
Matis BILLET <matis.billet@viacesi.fr>
2026-02-20 18:58:00 +0100

    finalisation de l'integration de gtest

---
+++ tests/test_basic.cpp
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+
+TEST(BasicTest, Addition) {
+    EXPECT_EQ(1 + 1, 2);
+}
+
+TEST(PokemonTest, ExempleEntity) {
+    EXPECT_TRUE(true);  // Remplace par tes vraies fonctions plus tard
+}
